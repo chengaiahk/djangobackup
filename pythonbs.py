95b9cc81b219feb276a82fa03fdd4d4a8e1f0065
chengaiahk <chengaiahk@newmeksolutions.com>
2021-12-31 16:30:34 +0530

    upd31

--- pythonbs.py
+++ pythonbs.py
@@ -3,3 +3,7 @@ print("ccccckkkkkkkk hello chengaiah")
 print("this is my first python code Second day for updating")
 
 print("third time changes")
+
+print("this is my first python code Second day for updating")
+
+print("third time changes")
